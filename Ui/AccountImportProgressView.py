cf34405430de4da09a3023f4ce4ae2b2df764aff
Aloxite <an_aurore@outlook.com>
2026-06-09 22:13:16 +0100

    Login detection for Firefox on Linux works while Firefox is running

--- Ui/AccountImportProgressView.py
+++ Ui/AccountImportProgressView.py
@@ -46,7 +46,7 @@ class AccountImportProgressView(QtWidgets.QDialog):
 
     def _showInfoText(self, browserName: str) -> None:
         if Utils.isLinux():
-            self._ui.info.setText(T("#The Twitch account saved in your {browserName} browser will be detected and linked.\nSince {appName} shares the same account information as your browser, signing out of your Twitch account in the browser will also sign you out of {appName}.\n\n\nBefore proceeding, please make sure that {browserName} is installed using the native package for your distribution and that you are signed in to Twitch.\n\nAlso, please close all {browserName} windows and terminate any running {browserName} processes.", browserName=browserName, appName=Config.APP_NAME))
+            self._ui.info.setText(T("#The Twitch account saved in your {browserName} browser will be detected and linked.\nSince {appName} shares the same account information as your browser, signing out of your Twitch account in the browser will also sign you out of {appName}.\n\nBefore proceeding, please make sure that {browserName} is installed using the native package for your distribution and that you are signed in to Twitch.", browserName=browserName, appName=Config.APP_NAME))
         else:
             self._ui.info.setText(T("#The Twitch account saved in your {browserName} browser will be detected and linked.\nSince {appName} shares the same account information as your browser, signing out of your Twitch account in the browser will also sign you out of {appName}.\n\n\nBefore proceeding, please make sure that {browserName} is installed and that you are signed in to Twitch.\n\nAlso, please close all {browserName} windows and terminate any running {browserName} processes.", browserName=browserName, appName=Config.APP_NAME))
 
